05030893ea7b9c791e2ca9f7d164ec124bfb1936
HancyWang <wanghouxun@126.com>
2018-05-07 11:18:55 +0800

    上位机发送14位SN到下位机，下位机接收存储之后，发送该SN到上位机，上位机进行比对，判断SN是否存储OK

---
+++ SN_Sender/SN_Sender/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace SN_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox_parity = new System.Windows.Forms.ComboBox();
+            this.comboBox_stopBit = new System.Windows.Forms.ComboBox();
+            this.comboBox_dataBits = new System.Windows.Forms.ComboBox();
+            this.comboBox_baud = new System.Windows.Forms.ComboBox();
+            this.comboBox_portName = new System.Windows.Forms.ComboBox();
+            this.button_serialPort_connect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox_SN_send = new System.Windows.Forms.ComboBox();
+            this.button_SN_send = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox_SN_recv = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox_parity);
+            this.groupBox1.Controls.Add(this.comboBox_stopBit);
+            this.groupBox1.Controls.Add(this.comboBox_dataBits);
+            this.groupBox1.Controls.Add(this.comboBox_baud);
+            this.groupBox1.Controls.Add(this.comboBox_portName);
+            this.groupBox1.Controls.Add(this.button_serialPort_connect);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 331);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Port";
+            // 
+            // comboBox_parity
+            // 
+            this.comboBox_parity.FormattingEnabled = true;
+            this.comboBox_parity.Location = new System.Drawing.Point(87, 212);
+            this.comboBox_parity.Name = "comboBox_parity";
+            this.comboBox_parity.Size = new System.Drawing.Size(112, 20);
+            this.comboBox_parity.TabIndex = 12;
+            // 
+            // comboBox_stopBit
+            // 
+            this.comboBox_stopBit.FormattingEnabled = true;
+            this.comboBox_stopBit.Location = new System.Drawing.Point(89, 167);
+            this.comboBox_stopBit.Name = "comboBox_stopBit";
+            this.comboBox_stopBit.Size = new System.Drawing.Size(112, 20);
+            this.comboBox_stopBit.TabIndex = 11;
+            // 
+            // comboBox_dataBits
+            // 
+            this.comboBox_dataBits.FormattingEnabled = true;
+            this.comboBox_dataBits.Location = new System.Drawing.Point(87, 124);
+            this.comboBox_dataBits.Name = "comboBox_dataBits";
+            this.comboBox_dataBits.Size = new System.Drawing.Size(112, 20);
+            this.comboBox_dataBits.TabIndex = 10;
+            // 
+            // comboBox_baud
+            // 
+            this.comboBox_baud.FormattingEnabled = true;
+            this.comboBox_baud.Location = new System.Drawing.Point(89, 81);
+            this.comboBox_baud.Name = "comboBox_baud";
+            this.comboBox_baud.Size = new System.Drawing.Size(112, 20);
+            this.comboBox_baud.TabIndex = 9;
+            // 
+            // comboBox_portName
+            // 
+            this.comboBox_portName.FormattingEnabled = true;
+            this.comboBox_portName.Location = new System.Drawing.Point(89, 44);
+            this.comboBox_portName.Name = "comboBox_portName";
+            this.comboBox_portName.Size = new System.Drawing.Size(112, 20);
+            this.comboBox_portName.TabIndex = 8;
+            // 
+            // button_serialPort_connect
+            // 
+            this.button_serialPort_connect.Location = new System.Drawing.Point(89, 252);
+            this.button_serialPort_connect.Name = "button_serialPort_connect";
+            this.button_serialPort_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_serialPort_connect.TabIndex = 7;
+            this.button_serialPort_connect.Text = "Connect";
+            this.button_serialPort_connect.UseVisualStyleBackColor = true;
+            this.button_serialPort_connect.Click += new System.EventHandler(this.button_serialPort_connect_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 252);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Parity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Stop Bit:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data Bits:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox_SN_send);
+            this.groupBox2.Controls.Add(this.button_SN_send);
+            this.groupBox2.Location = new System.Drawing.Point(253, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 112);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "发送到下位机";
+            // 
+            // comboBox_SN_send
+            // 
+            this.comboBox_SN_send.FormattingEnabled = true;
+            this.comboBox_SN_send.Location = new System.Drawing.Point(20, 42);
+            this.comboBox_SN_send.Name = "comboBox_SN_send";
+            this.comboBox_SN_send.Size = new System.Drawing.Size(236, 20);
+            this.comboBox_SN_send.TabIndex = 13;
+            // 
+            // button_SN_send
+            // 
+            this.button_SN_send.Location = new System.Drawing.Point(281, 41);
+            this.button_SN_send.Name = "button_SN_send";
+            this.button_SN_send.Size = new System.Drawing.Size(75, 23);
+            this.button_SN_send.TabIndex = 12;
+            this.button_SN_send.Text = "发送";
+            this.button_SN_send.UseVisualStyleBackColor = true;
+            this.button_SN_send.Click += new System.EventHandler(this.button_SN_send_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox_SN_recv);
+            this.groupBox3.Location = new System.Drawing.Point(253, 145);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(378, 112);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "从下位机读取";
+            // 
+            // textBox_SN_recv
+            // 
+            this.textBox_SN_recv.Location = new System.Drawing.Point(20, 48);
+            this.textBox_SN_recv.Name = "textBox_SN_recv";
+            this.textBox_SN_recv.ReadOnly = true;
+            this.textBox_SN_recv.Size = new System.Drawing.Size(236, 21);
+            this.textBox_SN_recv.TabIndex = 12;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.pictureBox2);
+            this.groupBox4.Location = new System.Drawing.Point(253, 265);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(378, 82);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "结果";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(372, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 376);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SN Sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_serialPort_connect;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_SN_send;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox_SN_recv;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox_parity;
+        private System.Windows.Forms.ComboBox comboBox_stopBit;
+        private System.Windows.Forms.ComboBox comboBox_dataBits;
+        private System.Windows.Forms.ComboBox comboBox_baud;
+        private System.Windows.Forms.ComboBox comboBox_portName;
+        private System.Windows.Forms.ComboBox comboBox_SN_send;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SN_Sender/SN_Sender/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SN_Sender
+{
+    public partial class Form1 : Form
+    {
+        private string[] m_old_serialPortNames;
+        private bool m_SerialPortOpened = false;
+        private List<byte> m_buffer = new List<byte>();
+
+        private const int HEAD = 0;
+        private const int LEN = 1;
+        private const int CMDTYPE = 2;
+        private const int FRAME_ID = 3;
+        private List<byte> m_SN_list = new List<byte>();
+
+        //private struct SN_BITS
+        //{
+        //    public byte SN_BIT_0;
+        //    public byte SN_BIT_1;
+        //    public byte SN_BIT_2;
+        //    public byte SN_BIT_3;
+        //    public byte SN_BIT_4;
+        //    public byte SN_BIT_5;
+        //    public byte SN_BIT_6;
+        //    public byte SN_BIT_7;
+        //    public byte SN_BIT_8;
+        //    public byte SN_BIT_9;
+        //    public byte SN_BIT_10;
+        //    public byte SN_BIT_11; 
+        //    public byte SN_BIT_12; 
+        //    public byte SN_BIT_13; 
+        //}
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Init_SerialPort()
+        {
+            string[] ports = SerialPort.GetPortNames();
+           
+            if (ports.Length != 0)
+            {
+                //Array.Sort(ports);
+                Array.Sort(ports, (a, b) => Convert.ToInt32(((string)a).Substring(3)).CompareTo(Convert.ToInt32(((string)b).Substring(3))));
+                m_old_serialPortNames = ports;
+                this.comboBox_portName.Items.AddRange(ports);
+                this.comboBox_portName.SelectedIndex = 0;
+            }
+
+            this.comboBox_baud.Text = "115200";
+            this.comboBox_dataBits.Text = "8";
+            this.comboBox_stopBit.Text = "one";
+            this.comboBox_parity.Text = "none";
+        }
+
+        private void LoadPicture()
+        {
+            if (!m_SerialPortOpened)
+            {
+                this.pictureBox1.Load(Environment.CurrentDirectory + @"\" + "red.bmp");
+            }
+            else
+            {
+                this.pictureBox1.Load(Environment.CurrentDirectory + @"\" + "green.bmp");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadPicture();
+            Init_SerialPort();
+        }
+
+        private void button_serialPort_connect_Click(object sender, EventArgs e)
+        {
+            //pictureBox2.Load(Environment.CurrentDirectory + @"\" + "fail.bmp");
+            m_SerialPortOpened = !m_SerialPortOpened;
+
+            if (m_SerialPortOpened)
+            {
+                try
+                {
+                    this.serialPort1.Open();
+                }
+                catch (Exception ex)
+                {
+                    m_SerialPortOpened = false;
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                this.button_serialPort_connect.Text = "Close";
+
+                m_SerialPortOpened = true;
+                this.comboBox_portName.Enabled = false;
+                LoadPicture();
+
+            }
+            else
+            {
+                this.button_serialPort_connect.Text = "Connect";
+                this.serialPort1.Close();
+                m_SerialPortOpened = false;
+                LoadPicture();
+                this.comboBox_portName.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] names = SerialPort.GetPortNames();
+
+            if (names.Length == 0)
+            {
+                return;
+            }
+            if (m_old_serialPortNames == null)
+            {
+                return;
+            }
+            //Array.Sort(names);
+            Array.Sort(names, (a, b) => Convert.ToInt32(((string)a).Substring(3)).CompareTo(Convert.ToInt32(((string)b).Substring(3))));
+            int nCount = 0;
+            if (names.Length == m_old_serialPortNames.Length)
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == m_old_serialPortNames[i])
+                    {
+                        nCount++;
+                    }
+                }
+                if (nCount == names.Length)  //如果每个都相同
+                {
+                    return;
+                }
+                else
+                {
+                    m_old_serialPortNames = names;  //如果不匹配，将新的值赋给旧的值
+                }
+            }
+            else
+            {
+                m_old_serialPortNames = names;
+            }
+
+            this.comboBox_portName.Items.Clear();
+
+            Array.Sort(names, (a, b) => Convert.ToInt32(((string)a).Substring(3)).CompareTo(Convert.ToInt32(((string)b).Substring(3))));
+
+            this.comboBox_portName.Items.AddRange(names);
+            this.comboBox_portName.SelectedIndex = 0; 
+        }
+
+        private void ParseFrame0x40()
+        {
+            int len = m_buffer.Count;
+            if (len != 14+4+2)
+            {
+                MessageBox.Show("下位机发送的SN不是14位!");
+                return;
+            }
+
+            //将接收到的SN放入m_SN_list中
+            for (int i = 4; i < len; i++)
+            {
+                m_SN_list[i - 4] = m_buffer[i];
+            }
+
+            textBox_SN_recv.Text = Convert.ToString(m_SN_list);
+            if (textBox_SN_recv.Text == comboBox_SN_send.Text.Trim())
+            {
+                pictureBox2.Load(Environment.CurrentDirectory + @"\" + "pass.bmp");
+                MessageBox.Show("Pass");
+            }
+            else
+            {
+                pictureBox2.Load(Environment.CurrentDirectory + @"\" + "fail.bmp");
+                MessageBox.Show("Fail");
+            }
+        }
+
+        private void ParseData2Lists()
+        {
+            //将数据解析挂入到3个链表中
+            if (m_buffer[CMDTYPE] != 0x00)
+            {
+                return;
+            }
+            //根据帧类型来判断
+            switch (m_buffer[FRAME_ID])
+            {
+                case 0x40:   //下位机返回0x40,携带SN
+                    ParseFrame0x40();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var nPendingRead = this.serialPort1.BytesToRead;
+            byte[] tmp = new byte[nPendingRead];
+            this.serialPort1.Read(tmp, 0, nPendingRead);
+
+            //m_bRcvParamtersCompleted = false;
+            lock (m_buffer)
+            {
+                m_buffer.AddRange(tmp);
+                #region
+                while (m_buffer.Count >= 4)
+                {
+                    if (m_buffer[HEAD] == 0xFF) //帧头
+                    {
+                        int len = Convert.ToInt32(m_buffer[LEN]); // 获取帧长度(不包含checksum1和checksum2)
+                        if (m_buffer.Count < len + 2)  //数据没有接收完全，继续接收
+                        {
+                            break;
+                        }
+                        int checksum = 256 * Convert.ToInt32(m_buffer[len]) + Convert.ToInt32(m_buffer[len + 1]);
+                        int sum = 0;
+                        for (int i = 1; i < len; i++) //校验和不包含包头
+                        {
+                            sum += Convert.ToInt32(m_buffer[i]);
+                        }
+                        //MessageBox.Show(sum.ToString());
+                        if (checksum == sum)
+                        {
+                            
+                            ParseData2Lists();
+                        }
+                        else
+                        {
+                            //校验之后发现数据不对,清除该帧数据
+                            m_buffer.RemoveRange(0, len + 2);
+                            continue;
+                        }
+                        m_buffer.RemoveRange(0, len + 2);
+                    }
+                    else
+                    {
+                        m_buffer.RemoveAt(0); //清除帧头
+                    }
+                }
+                #endregion
+            }
+        }
+
+        private void button_SN_send_Click(object sender, EventArgs e)
+        {
+            string str_send = comboBox_SN_send.Text;
+            //1.去除开头和结尾的空格字符
+            str_send = str_send.Trim();
+            //str_send = str_send.TrimStart();
+            //str_send = str_send.TrimEnd();
+
+            //MessageBox.Show(str_send);
+
+            //2.检查SN是否每一位都是0-9
+            foreach (var str in str_send)
+            {
+                if (Convert.ToInt32(str) >= 0 && Convert.ToInt32(str) <= 9)
+                {
+                    continue;
+                }
+                else
+                {
+                    MessageBox.Show("SN中不允许包含非法字符!");
+                    return;
+                }
+            }
+
+            //3. 检查SN的长度
+            if (str_send.Length != 14)
+            {
+                MessageBox.Show("请检查SN的长度!");
+                return;
+            }
+
+
+            if (!this.serialPort1.IsOpen)
+            {
+                MessageBox.Show("Please connect serial port first!");
+                return;
+            }
+
+            //发送SN到下位机
+            byte[] buffer = new byte[20];  //4+14+2
+            buffer[HEAD] = 0xFF;
+            buffer[LEN] = 0x12;   //4+14
+            buffer[CMDTYPE] = 0x01;
+            buffer[FRAME_ID] = 0x36;
+
+            int sum = 0;
+            for (int i = 1; i < Convert.ToInt32(buffer[LEN]); i++)
+            {
+                sum += buffer[i];
+            }
+            buffer[Convert.ToInt32(buffer[LEN])] = Convert.ToByte(sum / 256);   //checksum1
+            buffer[Convert.ToInt32(buffer[LEN]) + 1] = Convert.ToByte(sum % 256); //checksum2
+            this.serialPort1.Write(buffer, 0, Convert.ToInt32(buffer[LEN]) + 2);
+        }
+    }
+}
